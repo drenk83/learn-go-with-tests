27b6bbc07e21ef19ba020ef29ae89ef2f16356e3
Egor Chuprov <ytdrenk@gmail.com>
2026-01-12 17:17:07 +0300

    refactoring + add table tests

--- 05-structs-methods-and-interfaces/shapes_test.go
+++ 05-structs-methods-and-interfaces/shapes_test.go
@@ -32,3 +32,24 @@ func TestArea(t *testing.T) {
 		checkArea(t, circle, 314.1592653589793)
 	})
 }
+
+func TestAreaTable(t *testing.T) {
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{"Rectangle", Rectangle{12, 6}, 72.0},
+		{"Circle", Circle{10}, 314.1592653589793},
+		{"Triangle", Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %.2f want %.2f", got, tt.want)
+			}
+		})
+	}
+}
