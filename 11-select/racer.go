d91ddbb2119d79d8b4d4924243f50aa4d4c72934
Egor Chuprov <ytdrenk@gmail.com>
2026-01-19 21:11:24 +0300

    select

---
+++ 11-select/racer.go
@@ -0,0 +1,27 @@
+package select_test
+
+import (
+	"errors"
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (string, error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", errors.New("Time Out")
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
---
+++ 11-select/racer_test.go
@@ -0,0 +1,32 @@
+package select_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got, _ := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want: %q", got, want)
+	}
+}
